4b5fb5909cf41060a07e75f5f8d19cf29db6e8e3
Johan <142352311+h180240@users.noreply.github.com>
2026-03-18 18:09:58 +0100

    Icon fix ready_to_ship

--- atlantest/streamlit_pages/ready_to_ship.py
+++ atlantest/streamlit_pages/ready_to_ship.py
@@ -6,6 +6,6 @@ class Ready_To_Ship_Page(Base_Page):
 	def main(self):
 		st.write("# Ready to ship")
 		st.write("This page is under construction.")
-		st.set_page_config(page_title="Ready to Ship", page_icon=":material/package2:")
+		st.set_page_config(page_title="Ready to Ship", page_icon=":material/package_2:")
 		
 Ready_To_Ship_Page().main()
