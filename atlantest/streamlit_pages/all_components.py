70c6bba745709f7afd25565a91f1943e6fbd067b
Johan <142352311+h180240@users.noreply.github.com>
2026-03-18 18:29:48 +0100

    Refactor title components

--- atlantest/streamlit_pages/all_components.py
+++ atlantest/streamlit_pages/all_components.py
@@ -4,7 +4,8 @@ from atlantest.base_page import Base_Page
 
 class All_Components_Page(Base_Page):
 	def main(self):
-		st.write("# All components")
+		st.write("# Components - All")
 		st.write("This page is under construction.")
+		st.set_page_config(page_title="Components - All", page_icon=":material/list:")
 		
 All_Components_Page().main()
--- atlantest/streamlit_pages/ready_to_ship.py
+++ atlantest/streamlit_pages/ready_to_ship.py
@@ -4,8 +4,8 @@ from atlantest.base_page import Base_Page
 
 class Ready_To_Ship_Page(Base_Page):
 	def main(self):
-		st.write("# Ready to ship")
+		st.write("# Components - Ready to Ship")
 		st.write("This page is under construction.")
-		st.set_page_config(page_title="Ready to Ship", page_icon=":material/package_2:")
+		st.set_page_config(page_title="Components - Ready to Ship", page_icon=":material/package_2:")
 		
 Ready_To_Ship_Page().main()
