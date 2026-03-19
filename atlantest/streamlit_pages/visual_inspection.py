71c8f724439373c3ef112d5964be55a4b467f970
simsine <simen@simsine.no>
2026-03-19 17:17:43 +0100

    Add requirement to fill all radio buttons

--- atlantest/streamlit_pages/visual_inspection.py
+++ atlantest/streamlit_pages/visual_inspection.py
@@ -4,16 +4,14 @@ from atlantest.base_page import Base_Page
 
 class Visual_Inspection_Page(Base_Page):
 	def main(self):
-
 		st.set_page_config(page_title="Visual Inspection", page_icon=":material/visibility:")
 
-	with st.form("Visual Inspection Form"):
 		st.write("# Visual Inspection")
 		st.write(":red[This page is under construction.]")
 
 		input_code = st.text_input(
 			label = "Component serial number or test run number",
-			placeholder = ""
+			placeholder = "",
 		)
 
 		input_institution = st.selectbox(
@@ -21,95 +19,88 @@ class Visual_Inspection_Page(Base_Page):
 			options = ("UNIBERGEN", "UNIOSLO")
 		)
 
-		input_wirebond_pads_contamination_grade = st.radio(
-			label = "Wirebond pads clear of contamination (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_particulate_contamination_grade = st.radio(
-			label = "Particulate contamination (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_watermarks_grade = st.radio(
-			label = "Watermarks (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_scratches_grade = st.radio(
-			label = "Scratches (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_traces_grade = st.radio(
-			label = "Traces (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_soldermask_irregularities_grade = st.radio(
-			label = "Soldermask irregularities (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_hv_lv_connector_assembly_grade = st.radio(
-			label = "HV LV Data connector assembly issue (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_solder_spills_grade = st.radio(
-			label = "Solder spills (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_component_misalignment_grade = st.radio(
-			label = "Component misalignment (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_shorts_or_close_proximity_grade = st.radio(
-			label = "Shorts or close proximity (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_opens_tombstoning_grade = st.radio(
-			label = "Tombstone or misalignment (grade 1-3)",
-			options = (1, 2, 3),
-			horizontal = True,
-			index = None
-		)
-
-		input_overall_grade = st.radio(
-			label = "Overall grade (1 no damages, 2 no action, 3 re-clean, 4 rework, 5 discard)",
-			options = (1, 2, 3, 4, 5),
-			horizontal = True,
-			index = None
-		)
+		range_input_fields = [
+			st.radio(
+				label = "Wirebond pads clear of contamination (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Particulate contamination (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Watermarks (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Scratches (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Traces (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Soldermask irregularities (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "HV LV Data connector assembly issue (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Solder spills (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Component misalignment (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Shorts or close proximity (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Tombstone or misalignment (grade 1-3)",
+				options = (1, 2, 3),
+				horizontal = True,
+				index = None
+			),
+			st.radio(
+				label = "Overall grade (1 no damages, 2 no action, 3 re-clean, 4 rework, 5 discard)",
+				options = (1, 2, 3, 4, 5),
+				horizontal = True,
+				index = None
+			)
+		]
 
 		input_observation = st.text_area(
 			label = "Observations",
 		)
 		
-		st.form_submit_button()
-	
-		
+		ALL_RANGE_FIELDS_FILLED = all(range_input_fields)
+
+		if st.button(label = "Submit test", disabled = not ALL_RANGE_FIELDS_FILLED):
+			st.write("yay!")
+
 Visual_Inspection_Page().main()
